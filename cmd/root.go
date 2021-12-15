79fb80f36d447a14b613ddb150044739fb86dc3d
Shitomo <c.adaeibf@icloud.com>
2021-12-16 00:19:07 +0900

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "sample-cli",
+	Short: "要約を書くところです",
+	Long: `改行を含む長い文章を書くところです。使い方とか書いてください
+
+これはサンプルCLIです
+
+- 
+
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("root command")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sample-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github/Shitomo/sample-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
